fcd32211da0b897f5401a962e1bcddb39f470cc5
tabloid <tabloid@65644016-39b1-43b1-bf79-96bc8fe82c15>
2014-12-04 16:07:51 +0000

    04-dec-2014 Add parsing improvement of config files for LINUX case: ignoring trailing slash in path variables ($fbc and $tmpdir). Adjust descriptions of all config files - add title info.

--- qa/oltp-emul/src/oltp25_config.nix
+++ qa/oltp-emul/src/oltp25_config.nix
@@ -1,15 +1,15 @@
-# Configuration parameters for OLTP-EMUL test for parsing by
-# LINUX command scenario '1run_oltp_emul.sh' and launching 
-# test against Firebird 2.5
-
 ####################################################################
+# OLTP-EMUL test for Firebird 2.5 & 3.0 - configuration parameters.
+# This file is used for running ISQL sessions on LINUX machine
+# and test Firebird 2.5.
+# Parameters are extracted by '1run_oltp_emul.sh' command scenario.
 # Do *NOT* remove or make empty (undefined) any of these parameters.
 ####################################################################
 
-# Folder with Firebird console utilities (do NOT use final backslash here).
+# Folder with Firebird console utilities, with or w/o trailing slash.
 fbc=/opt/fb25sc/bin
 
-# Folder where to store work and error logs (do NOT use final backslash here):
+# Folder where to store work and error logs, with or w/o trailing backslash.
 tmpdir=/var/tmp/logs.oltp25
 
 # is_embed=1 - if Firebird runs in embedded mode, otherwise 0
@@ -17,18 +17,20 @@ is_embed=0
 
 # Alias or full path and file name of database.
 # If you want this database be created by test itself, specify it as
-# FULL PATH and file name. No spaces or non-latin characters can be here!
-dbnm = /var/db/fb25/oltp25b.fdb
+# full path and file name. No spaces or non-latin characters can be here!
+dbnm = /var/db/fb25/oltp25.fdb
 
 # Should set transaction command include NO AUTO UNDO clause ?
 # Recommended value: 1
 no_auto_undo=1
 
-# Do we use tee/supertee utility to provide timestamps for error messages 
-# before they are logged in .err files ?
+# Do we use tee.exe utility to provide timestamps for error messages before they
+# are logged in .err files ? 
+# (Windows only. Not implemented for Linux, please leave = 0)
 use_mtee=0
 
 # Add in ISQL logs detailed info for each iteration (select from perf_log...) ?
+# Recommended value: 0
 # Note: value = 1 will increase workload on DISK on client machine.
 # Do not use if you are not interested on data of table 'perf_log'.
 detailed_info=0
@@ -39,17 +41,15 @@ detailed_info=0
 # Recommended value: at least 30000 
 init_docs=30000
 
-
-# Number of pages for usage during init data population ("-c" switch for ISQL).
-# Actual only for CS and SC, will be ignored in SS. Used ONLY during phase of
-# initial data population and is IGNORED on main phase of test.
-# Make sure that it is LESS than FileSystemCacheThreshold, which default is 65536.
+# Number of pages for usage during init data population ("-c" switch for ISQL),
+# actual only for CS and SC, will be ignored in SS.
+# (check that it is LESS than FileSystemCacheThreshold, which default is 65536)
 init_buff=32768
 
-# Should script be PAUSED after finish creating <init_docs> documents ?
-# Value = 1 can be set for making copy of .fdb and restore initial database
-# it on the following runs thus avoiding creation of <init_docs> again:
-wait_for_copy=1
+# Should script be PAUSED after finish creating <init_docs> documents
+# (for making copy of .fdb and restore it on the following runs thus
+# avoiding to make init_docs again):
+wait_for_copy=0
 
 # Time (in minutes) to warm-up database after initial data population
 # will finish and before all following operations will be measured:
@@ -58,10 +58,11 @@ warm_time=20
 
 # Limit (in minutes) to measure operations before test autostop itself:
 # Recommended value: at least 60
-test_time=60
+test_time=180
 
-# Rest params are not needed if embedded mode but should be non-empty 
-# for this command scenario:
+# Rest params not needed if embedded mode 
+# but should be non-empty to run batch:
+
 usr=SYSDBA
 pwd=masterke
 host=192.168.0.220
--- qa/oltp-emul/src/oltp30_config.nix
+++ qa/oltp-emul/src/oltp30_config.nix
@@ -1,31 +1,32 @@
-# Configuration parameters for OLTP-EMUL test for parsing by
-# LINUX command scenario '1run_oltp_emul.sh' and launching 
-# test against Firebird 3.0
-
 ####################################################################
+# OLTP-EMUL test for Firebird 2.5 & 3.0 - configuration parameters.
+# This file is used for running ISQL sessions on LINUX machine
+# and test Firebird 3.0.
+# Parameters are extracted by '1run_oltp_emul.sh' command scenario.
 # Do *NOT* remove or make empty (undefined) any of these parameters.
 ####################################################################
 
-# Folder with Firebird console utilities (do NOT use final backslash here):
+# Folder with Firebird console utilities, with or w/o trailing slash.
 fbc=/opt/fb30trnk/bin
 
-# Folder where to store work and error logs (do NOT use final backslash here):
-tmpdir=/var/tmp/logs.oltp30	
+# Folder where to store work and error logs, with or w/o trailing backslash.
+tmpdir=/var/tmp/logs.oltp30
 
 # is_embed=1 - if Firebird runs in embedded mode, otherwise 0
 is_embed=0
 
 # Alias or full path and file name of database.
 # If you want this database be created by test itself, specify it as
-# FULL PATH and file name. No spaces or non-latin characters can be here!
-dbnm=/var/db/fb30/oltp30c.fdb
+# full path and file name. No spaces or non-latin characters can be here!
+dbnm=/var/db/fb30/oltp30.fdb
 
-# Should 'SET TRANSACTION' command include NO AUTO UNDO clause ?
+# Should set transaction command include NO AUTO UNDO clause ?
 # Recommended value: 1
 no_auto_undo=1
 
-# Do we use tee/supertee utility to provide timestamps for error messages 
-# before they are logged in .err files ?
+# Do we use tee.exe utility to provide timestamps for error messages before they
+# are logged in .err files ? 
+# (Windows only. Not implemented for Linux, please leave = 0)
 use_mtee=0
 
 # Add in ISQL logs detailed info for each iteration (select from perf_log...) ?
@@ -36,10 +37,10 @@ detailed_info=0
 
 # Do we add call to mon$ tables before and after each application unit 
 # in generated file tmp_random_run.sql ? 
-# Actual only for 3.0; usage of this setting see in 1run_oltp_emul.sh.
+# Actual only for 3.0; usage of this setting see in 1run_oltp_emul.bat.
 # NOTE: if this setting = 1, then you have also to correct table 'settings':
 # update settings set svalue='1' where mcode='ENABLE_MON_QUERY';
-mon_unit_perf=0
+mon_unit_perf=1
 
 # Number of documents, total of all types, for initial data population.
 # Command scenario will compare number of existing document with this
@@ -59,16 +60,16 @@ init_buff=32768
 wait_for_copy=0
 
 # Time (in minutes) to warm-up database after initial data population
-# will finish and before all following operations will be measured.
+# will finish and before all following operations will be measured:
 # Recommended value: at least 10 for Firebird 3.0
 warm_time=10
 
-# Limit (in minutes) to measure operations before test autostop itself.
+# Limit (in minutes) to measure operations before test autostop itself:
 # Recommended value: at least 60
-test_time=60
+test_time=180
 
-# Rest params are not needed if embedded mode but should be non-empty 
-# for this command scenario:
+# Rest params are not needed if embedded mode 
+# but should be non-empty to run batch:
 usr=SYSDBA  		
 pwd=masterke     		
 host=192.168.0.220
