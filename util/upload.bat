17694e9184c0528da7e028993cde24ce08336eaf
pavel-zotov <p519446@yandex.ru>
2020-11-08 12:06:17 +0300

    Removed old files before upload new version.

--- util/upload.bat
+++
@@ -1,3 +0,0 @@
-@rem Replace this command with appropriate one that will post report from %tmpdir% folder
-@rem to some remote storage: 
-@rem <path>\curl.exe -F "name=%1" -F "file=@%2" http://foo.com/reports/import_log_scenario
